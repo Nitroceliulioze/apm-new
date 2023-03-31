a383a9269825dc0135a7e46066f025071c9141a5
Nitroceliulioze <aksionova.diana@gmail.com>
2023-03-31 10:20:06 +0300

    add formbuilder

--- src/app/customer/customer.component.ts
+++ src/app/customer/customer.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 import { Customer } from './customer';
 
@@ -11,20 +11,31 @@ import { Customer } from './customer';
 export class CustomerComponent implements OnInit {
   customerForm!: FormGroup;
   customer = new Customer();
+  emailMessage = '';
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
-    this.customerForm = new FormGroup({
-      firstName: new FormControl(),
-      lastName: new FormControl(),
-      emai: new FormControl(),
-      sendCatalog: new FormControl(true)
-    });
+    this.customerForm = this.fb.group({
+      firstName: "",
+      lastName: "",
+      email: "",
+      sendCatalog: true
+    }) 
   }
 
   save(): void {
     console.log(this.customerForm);
     console.log('Saved: ' + JSON.stringify(this.customerForm));
+  }
+
+  populateTestData():void {
+    this.customerForm.patchValue({
+      firstName: 'Diana',
+      lastName: 'Zukauskiene',
+      // email: 'diana@test.com',
+      sendCatalog: false
+    })
+
   }
 }
